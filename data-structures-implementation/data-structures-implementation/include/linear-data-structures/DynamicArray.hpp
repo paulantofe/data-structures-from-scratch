88f3d16a25018d4c6f3f8ccf4b89278ffdd80a4f
Paul Antofe <paul.antofe@gmail.com>
2026-05-20 16:28:26 +0300

    Fixed some other bugs

--- data-structures-implementation/data-structures-implementation/include/linear-data-structures/DynamicArray.hpp
+++ data-structures-implementation/data-structures-implementation/include/linear-data-structures/DynamicArray.hpp
@@ -3,124 +3,135 @@
 
 #include <iostream>
 #include <stdexcept>
+#include <algorithm>
 
 template <typename T> class DynamicArray {
 private:
-	T* arr;
-	unsigned int capacity;
-	unsigned int size;
+	T* m_arr;
+	unsigned int m_capacity;
+	unsigned int m_size;
 
 	void doubleCapacity() {
-		T* temp = new T[2 * capacity];
+		T* temp = new T[2 * m_capacity];
 
-		for (int i = 0; i < size; i++)
-			temp[i] = arr[i];
+		for (unsigned int i = 0; i < m_size; i++)
+			temp[i] = m_arr[i];
 
-		delete[] arr;
-		capacity *= 2;
-		arr = temp;
+		delete[] m_arr;
+		m_capacity *= 2;
+		m_arr = temp;
 	}
 
 public:
-	DynamicArray() : capacity(1), size(0) { arr = new T[capacity]; }
+	DynamicArray() : m_capacity(1), m_size(0) { m_arr = new T[m_capacity]; }
 
-	DynamicArray(const unsigned int initial_size) : capacity(size), size(initial_size) {
-		arr = new T[size];
+	DynamicArray(const unsigned int initial_size) : m_capacity(initial_size), m_size(initial_size) {
+		m_arr = new T[size];
 	}
 
 	DynamicArray(const DynamicArray& other) {
-		capacity = other.capacity;
-		size = other.size;
+		m_capacity = other.m_capacity;
+		m_size = other.m_size;
 
-		arr = new T[capacity];
-		for (int i = 0; i < size; i++)
-			arr[i] = other.arr[i];
+		m_arr = new T[m_capacity];
+		for (unsigned int i = 0; i < m_size; i++)
+			m_arr[i] = other.m_arr[i];
 	}
 
 	DynamicArray& operator=(const DynamicArray& other) {
 		if (this == &other)
 			return *this;
 
-		delete[] arr;
-		capacity = other.capacity;
-		size = other.size;
+		delete[] m_arr;
+		m_capacity = other.m_capacity;
+		m_size = other.m_size;
 
-		arr = new T[capacity];
-		for (int i = 0; i < size; i++)
-			arr[i] = other.arr[i];
+		m_arr = new T[m_capacity];
+		for (unsigned int i = 0; i < m_size; i++)
+			m_arr[i] = other.m_arr[i];
 
 		return *this;
 	}
 
-	~DynamicArray()  { delete[] arr; } 
+	T& operator[](const unsigned int index) {
+		return m_arr[index];
+	}
+
+	const T& operator[](const unsigned int index) const {
+		return m_arr[index];
+	}
+
+	~DynamicArray()  { delete[] m_arr; } 
 
 	void push_back(const T& value) {
-		if (size == capacity)
+		if (m_size == m_capacity)
 			doubleCapacity();
 
-		arr[size++] = value;
+		m_arr[m_size++] = value;
 	} 
 
 	void pop_back() {
-		size--;
+		if (m_size == 0)
+			return;
+		m_size--;
 	} 
 
-	void remove(const int index) {
-		if (index < 0 || index >= size)
+	void remove(const unsigned int index) {
+		if (index >= m_size)
 			throw std::out_of_range("Index is out of range.");
 
-		for (int i = index; i < size - 1; i++)
-			arr[i] = arr[i + 1];
-		size--;
+		for (unsigned int i = index; i < m_size - 1; i++)
+			m_arr[i] = m_arr[i + 1];
+		m_size--;
 	} 
 
-	void push(const T& value, int index) {
-		if (index < 0 || index > size)
+	void push(const T& value, const unsigned int index) {
+		if (index > m_size)
 			throw std::out_of_range("Index out of bounds for insertion.");
 
-		if (size == capacity)
+		if (m_size == m_capacity)
 			doubleCapacity();
 
-		for (int i = size; i > index; i--)
-			arr[i] = arr[i - 1];
+		for (unsigned int i = m_size; i > index; i--)
+			m_arr[i] = m_arr[i - 1];
 
-		arr[index] = value;
-		size++;
+		m_arr[index] = value;
+		m_size++;
 	} 
 
 	void print() const {
-		for (int i = 0; i < size; i++)
-			std::cout << arr[i] << ' ';
+		for (unsigned int i = 0; i < m_size; i++)
+			std::cout << m_arr[i] << ' ';
 		std::cout << std::endl;
 	} 
 
 	void clear() {
-		size = 0;
+		m_size = 0;
 	} 
 
 	unsigned int size() const {
-		return size;
+		return m_size;
 	}
 
 	unsigned int capacity() const {
-		return capacity;
+		return m_capacity;
 	}
 
 	bool empty() const {
-		return size == 0;
+		return m_size == 0;
 	}
 
 	void resize(const unsigned int newSize) {
-		T* temp = new T[newSize];
+		T* temp = new T[newSize]();
 
-		for (int i = 0; i < std::min(size, newSize); i++)
-			temp[i] = arr[i];
+		for (unsigned int i = 0; i < std::min(m_size, newSize); i++)
+			temp[i] = m_arr[i];
 
-		delete[] arr;
+		delete[] m_arr;
 
-		arr = temp;
-		size = newSize;
-		capacity = newSize;
+		m_arr = temp;
+		m_size = newSize;
+		m_capacity = newSize;
 	}
 
 };
